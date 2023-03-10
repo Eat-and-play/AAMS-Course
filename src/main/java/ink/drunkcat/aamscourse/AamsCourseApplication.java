b87808772955b821491b0660e90cefd1fde1def1
Tuxzx <txzx@outlook.com>
2023-03-10 20:48:39 +0800

    init

---
+++ src/main/java/ink/drunkcat/aamscourse/AamsCourseApplication.java
@@ -0,0 +1,13 @@
+package ink.drunkcat.aamscourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AamsCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AamsCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ink/drunkcat/aamscourse/AamsCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package ink.drunkcat.aamscourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AamsCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
